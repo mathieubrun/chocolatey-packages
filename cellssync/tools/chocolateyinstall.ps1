021089b6eb2cbdf60c5e82c92c8752ac325e03ec
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2022-02-16 11:43:14 +0100

    adding cellssync package

---
+++ cellssync/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url64 = 'https://download.pydio.com/pub/cells-sync/release/0.9.2/windows-amd64/CellsSync-0.9.2.msi'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'MSI'
+  url64bit       = $url64
+  softwareName   = 'cellssync*'
+  checksum64     = 'C61545A8B3DABC670A02974AC6782CE86DF796B14FCBA12AC74BF367BA46CB49'
+  checksumType64 = 'sha256'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
