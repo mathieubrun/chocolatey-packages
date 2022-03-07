24360fa61c1abd60a192c42ee69ed5ae5a0e83a7
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2022-03-07 16:21:28 +0100

    adding rancher desktop package

---
+++ rancher-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.1.1/Rancher.Desktop.Setup.1.1.1.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'Rancher Desktop*'
+  checksum       = 'DD3D52501963FD1757E8D0B972DEDA264AFE38D8F0EF3383AAA5B1BD6B6C0747'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
