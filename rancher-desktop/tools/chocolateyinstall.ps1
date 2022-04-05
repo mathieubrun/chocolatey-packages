873cf8a3ec3965683209f0ce33f799174a3078f7
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2022-04-05 16:38:19 +0200

    upgrade to version 1.2.1 for rancher desktop

--- rancher-desktop/tools/chocolateyinstall.ps1
+++ rancher-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.1.1/Rancher.Desktop.Setup.1.1.1.exe'
+$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.2.1/Rancher.Desktop.Setup.1.2.1.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url            = $url
   softwareName   = 'Rancher Desktop*'
-  checksum       = 'DD3D52501963FD1757E8D0B972DEDA264AFE38D8F0EF3383AAA5B1BD6B6C0747'
+  checksum       = '3711e91ae84140a429d012b3a9bba19e87fab3e1929eb4faca2620dc7ec11d71'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
