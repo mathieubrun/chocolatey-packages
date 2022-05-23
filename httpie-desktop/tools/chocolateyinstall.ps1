6a5e1fc73dcb4fccfbfc89ce2c73b189e8bf200a
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2022-05-23 21:20:31 +0200

    AU: httpie-desktop version 2022.9.1

--- httpie-desktop/tools/chocolateyinstall.ps1
+++ httpie-desktop/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/httpie/desktop/releases/download/v2022.8.0/HTTPie-Setup-2022.8.0.exe'
+$url = 'https://github.com/httpie/desktop/releases/download/v2022.9.1/HTTPie-Setup-2022.9.1.exe'
 $pp = Get-PackageParameters
 
 $args = '/S'
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType       = 'EXE'
   url            = $url
   softwareName   = 'HTTPie*'
-  checksum       = '15f0cc479687951bf1e872f821e0aca91099fe7b66b37ec5723b80825c347fa5'
+  checksum       = '62e458b005fe55c863b2e1b682d6d7df77c65552130491b3247513489beb1946'
   checksumType   = 'sha256'
   silentArgs     = $args
   validExitCodes = @(0)
