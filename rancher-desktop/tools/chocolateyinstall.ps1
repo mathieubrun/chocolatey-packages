d96604238178057007a0977cf74ed1157f043ef7
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2022-06-17 14:51:29 +0200

    AU: rancher-desktop 1.4.1

--- rancher-desktop/tools/chocolateyinstall.ps1
+++ rancher-desktop/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.3.0/Rancher.Desktop.Setup.1.3.0.exe'
+$url = 'https://github.com/rancher-sandbox/rancher-desktop/releases/download/v1.4.1/Rancher.Desktop.Setup.1.4.1.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url            = $url
   softwareName   = 'Rancher Desktop*'
-  checksum       = '92108cbbd8c98f99b00a608d8f7d21e12faeca76f16890585ef212cc5bf1c779'
+  checksum       = '39d79e4287998e483e387166b1447a7df78e441d6cb939166dcf9cd9436adc31'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
