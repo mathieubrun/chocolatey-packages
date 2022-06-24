4dc707d9f89a174eb059d3ddafbd2d471a14cc00
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2022-06-24 17:52:54 +0200

    AU: gmsh version 4.10.4

--- gmsh/tools/chocolateyinstall.ps1
+++ gmsh/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://gmsh.info/bin/Windows/gmsh-4.10.2-Windows64.zip'
+$url64 = 'https://gmsh.info/bin/Windows/gmsh-4.10.4-Windows64.zip'
 $installDir = 'C:\gmsh'
-$version = '4.10.2'
+$version = '4.10.4'
 $dirToExtract = "gmsh-$version-Windows64"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $installDir
   url64bit       = $url64
-  checksum64     = '924d0a3819479eb444647c21a06049957af87a65dd64b7f731802c11f83dd862'
+  checksum64     = '6f8c75d8447923f77f47b82aeb5ffe9671b74a75c5cfb936c7d47349cbe58276'
   checksumType64 = 'sha256'
 }
 
