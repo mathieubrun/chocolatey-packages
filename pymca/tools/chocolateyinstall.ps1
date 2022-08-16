0e42d5fc5cb3281b4331ab2808c68c074d0bbbba
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2022-08-16 10:50:49 +0200

    adding pymca package

---
+++ pymca/tools/chocolateyinstall.ps1
@@ -0,0 +1,53 @@
+﻿$ErrorActionPreference = 'Stop'
+$url64 = 'https://sourceforge.net/projects/pymca/files/pymca/PyMca 5.7.4/pymca5.7.4-win64.exe'
+$version = [version]'5.7.4'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url64bit       = $url64
+    softwareName   = 'pymca*'
+    checksum64     = '35d8d95b52bec59d62d57df4e25daa70ceaf0ec565390564038fddd867e0102a'
+    checksumType64 = 'sha256'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+}
+
+#Uninstalls the previous version of PyMca if either version exists
+Write-Output 'Searching if the previous version exists...'
+
+[array]$checkreg = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($checkreg.Count -eq 0) {
+    Write-Output 'No installed old version. Process to install PyMca.'
+    # No version installed, process to install
+    $ExecInstall = $true
+} elseif ($checkreg.count -ge 1) {
+    $checkreg | ForEach-Object {
+        if ($null -ne $_.PSChildName) {
+            if ([version]$_.DisplayVersion -lt $version) {
+                Write-Output "Uninstalling PyMca previous version : $($_.DisplayVersion)"
+                $packageArgs['file'] = "$($_.UninstallString)"
+
+                # Uninstall previous version
+                Uninstall-ChocolateyPackage @packageArgs
+        
+
+                # Process to install
+                Write-Output 'Installing new version of PyMca'
+                $ExecInstall = $true
+            } elseif (([version]$_.DisplayVersion -eq $version) -and ($env:ChocolateyForce)) {
+                Write-Output "PyMca $version already installed, but --force option is passed, download and install"
+                $ExecInstall = $true
+            } else {
+                Write-Output 'Newest version or same version of PyMca is already installed, skip install'
+                $ExecInstall = $false
+            }
+        }
+    }
+}
+
+# Check if install is required
+if ($ExecInstall) {
+    Install-ChocolateyPackage @packageArgs
+}
