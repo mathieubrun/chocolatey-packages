ac391409b925b9bd51e1ff9302bebf50d9f2fcb4
Nicolas ROGIER <nicolas.rogier@univ-lorraine.fr>
2022-09-30 14:21:11 +0200

    fix get latest url binary github (rancher-desktop)

--- rancher-desktop/update.ps1
+++ rancher-desktop/update.ps1
@@ -1,11 +1,10 @@
 Import-Module AU
 
-$releases = 'https://github.com/rancher-sandbox/rancher-desktop/releases/latest'
-$domain = 'https://github.com'
+$releases = 'https://api.github.com/repos/rancher-sandbox/rancher-desktop/releases/latest'
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
+        'tools\chocolateyinstall.ps1' = @{
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
@@ -13,14 +12,14 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $latest_asset = (Invoke-RestMethod -Uri $releases).assets
 
-    $url = $download_page.Links | Where-Object href -match 'Rancher\.Desktop\.Setup\..*\.exe' | Select-Object -First 1 -ExpandProperty href
+    $url = $latest_asset | Where-Object browser_download_url -Match 'Rancher\.Desktop\.Setup\..*\.exe' | Select-Object -First 1 -ExpandProperty browser_download_url
     $version = Get-Version $url
 
     return @{
-        Version  = $version
-        URL32    = "${domain}${url}"
+        Version = $version
+        URL32   = $url
     }
 
 }
