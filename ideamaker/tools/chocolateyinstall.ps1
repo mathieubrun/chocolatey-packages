115037ba6a1d57cda3dd5cdcd16a16d3dd0bb6e9
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2022-12-13 11:15:20 +0100

    movez ideamaker pakg to chocolatey-packages

---
+++ ideamaker/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$url = 'https://download.raise3d.com/ideamaker/release/4.2.3/install_ideaMaker_4.2.3.5600.exe'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url            = $url
+    softwareName   = 'ideamaker*'
+    checksum       = 'bf52a8f763a6b1d5e00ff1fce18085aa5147f8b3d57f80c3fb02fa7f23b0fb75'
+    checksumType   = 'sha256'
+    silentArgs     = '/S'
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
